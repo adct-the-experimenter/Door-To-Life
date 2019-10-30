fd772f1bd6b35e401204296ada4330fe2d1e8eb8
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-10-29 18:46:40 -0700

    Made enemies stronger for a challenge.

--- src/damage_values.cpp
+++ src/damage_values.cpp
@@ -17,7 +17,7 @@ std::int16_t cockroach_PushBackHero = 30; // 30 pixel push back distance
 std::int16_t cockroach_HitBySwordDamage = 20; // damage to cockroach when hit by sword
 std::int16_t cockroach_PushBackBySword = 40; // 40 pixel push back distance
 
-std::int16_t cockroach_HitByBulletDamage = 20;
+std::int16_t cockroach_HitByBulletDamage = cockroachHealth / 5;
 std::int16_t cockroach_PushBackByBullet = 40;
 
 //Greedy zombie
@@ -29,5 +29,5 @@ std::int16_t greedZombie_PushBackHero = 30; // 30 pixel push back distance
 std::int16_t greedZombie_HitBySwordDamage = 20; // damage to greedZombie when hit by sword
 std::int16_t greedZombie_PushBackBySword = 40; // 40 pixel push back distance
 
-std::int16_t greedZombie_HitByBulletDamage = 20;
+std::int16_t greedZombie_HitByBulletDamage = greedZombieHealth / 7;
 std::int16_t greedZombie_PushBackByBullet = 40;
